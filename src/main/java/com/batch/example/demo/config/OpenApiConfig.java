69e107fdec7422f88ef670c3b73badf22e3e8bf7
Mateo Calcagno <mateo55_malunies@hotmail.com>
2025-10-17 18:53:31 -0300

    Rename application.properties to application.yml and update configuration settings

---
+++ src/main/java/com/batch/example/demo/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.batch.example.demo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Example API")
+                        .description("Cliente interno que consume la API pública de cotizaciones del dólar en Argentina.")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Tu Nombre")
+                                .url("https://tusitio.com")
+                                .email("tuemail@ejemplo.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")));
+    }
+}
